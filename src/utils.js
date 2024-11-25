052b5a39bca9c8fff3fcafcc9c7f5db71852284b
lohi212 <suvvarilohita@gmail.com>
2024-11-25 16:00:55 +0530

    feat: mapper obj

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+export const mapperObj = {
+  Rock: {
+    img: "/images/fist.png",
+    key: "rock",
+    win: "Scissor",
+  },
+  Paper: {
+    img: "/images/paper.png",
+    key: "paper",
+    win: "Rock",
+  },
+  Scissor: {
+    img: "/images/scissor.png",
+    key: "scissor",
+    win: "Paper",
+  },
+};
